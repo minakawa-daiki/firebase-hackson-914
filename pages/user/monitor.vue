c76f96d23985538bee7a23c6f1e67f96348d556e
shiba mitsue <mitue.okamoto@gmail.com>
2019-09-14 14:29:28 +0900

    HeartBeats / BloodSugar

---
+++ pages/user/monitor.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-container>
+      <BloodSugar />
+      <HeartBeats />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import BloodSugar from '@/components/BloodSugar'
+import HeartBeats from '@/components/HeartBeats'
+export default {
+  components: {
+    BloodSugar,
+    HeartBeats
+  }
+}
+</script>
