5ae374d2ce9edc11d7a67f5197a8bc1ad5cff721
shiba mitsue <mitue.okamoto@gmail.com>
2019-09-14 15:20:17 +0900

    ModelCase Mock

---
+++ pages/user/daily-schedule.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-container class="flex">
+      <model-case class="model-case" />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import ModelCase from '@/components/ModelCase'
+export default {
+  components: {
+    ModelCase
+  }
+}
+</script>
