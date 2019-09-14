33622c7cab28cf3cc93966ff37a952224305ccbd
minakawa-daiki <minakawa.daiki.as@gmail.com>
2019-09-14 16:52:01 +0900

    model case

--- components/ModelCase.vue
+++ components/ModelCase.vue
@@ -1,26 +1,105 @@
 <template>
   <v-container>
-    <h2>Model Case</h2>
+    <h2>スケジュール</h2>
     <v-timeline align-top dense>
       <v-timeline-item color="pink" small>
         <v-row class="pt-1">
           <v-col cols="3">
-            <strong>5pm</strong>
+            <strong>6:30</strong>
           </v-col>
           <v-col>
-            <strong>New Icon</strong>
-            <div class="caption">Mobile App</div>
+            <strong>起床</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>6:45</strong>
+          </v-col>
+          <v-col>
+            <strong>準備</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>7:15</strong>
+          </v-col>
+          <v-col>
+            <strong>朝食</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>7:45</strong>
+          </v-col>
+          <v-col>
+            <strong>片付け</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>8:00</strong>
+          </v-col>
+          <v-col>
+            <strong>出勤</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>9:00</strong>
+          </v-col>
+          <v-col>
+            <strong>仕事</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>12:00</strong>
+          </v-col>
+          <v-col>
+            <strong>昼食</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>13:00</strong>
+          </v-col>
+          <v-col>
+            <strong>仕事</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>18:00</strong>
+          </v-col>
+          <v-col>
+            <strong>移動</strong>
           </v-col>
         </v-row>
       </v-timeline-item>
       <v-timeline-item color="teal lighten-3" small>
         <v-row class="pt-1">
           <v-col cols="3">
-            <strong>3-4pm</strong>
+            <strong>18:30</strong>
           </v-col>
           <v-col>
-            <strong>Design Stand Up</strong>
-            <div class="caption mb-2">Hangouts</div>
+            <strong>トレーニング</strong>
+            <div class="caption mb-2">With</div>
             <v-avatar>
               <v-img
                 src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Kurt&hairColor=Red&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Skull&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown"
@@ -42,24 +121,68 @@
       <v-timeline-item color="pink" small>
         <v-row class="pt-1">
           <v-col cols="3">
-            <strong>12pm</strong>
+            <strong>19:30</strong>
           </v-col>
           <v-col>
-            <strong>Lunch break</strong>
+            <strong>移動</strong>
           </v-col>
         </v-row>
       </v-timeline-item>
-      <v-timeline-item color="teal lighten-3" small>
+      <v-timeline-item color="pink" small>
         <v-row class="pt-1">
           <v-col cols="3">
-            <strong>9-11am</strong>
+            <strong>20:00</strong>
           </v-col>
           <v-col>
-            <strong>Finish Home Screen</strong>
-            <div class="caption">Web App</div>
+            <strong>夕食</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>21:00</strong>
+          </v-col>
+          <v-col>
+            <strong>帰宅</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>21:30</strong>
+          </v-col>
+          <v-col>
+            <strong>風呂</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>22:00</strong>
+          </v-col>
+          <v-col>
+            <strong>趣味</strong>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>23:30</strong>
+          </v-col>
+          <v-col>
+            <strong>就寝</strong>
           </v-col>
         </v-row>
       </v-timeline-item>
     </v-timeline>
   </v-container>
 </template>
+<style>
+.v-timeline-item__divider {
+  margin-top: 16px;
+}
+</style>
